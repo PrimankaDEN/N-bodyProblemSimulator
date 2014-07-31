d65bf6f3d91966d95e848f1f8b98851e959322ba
PrimankaDEN <primanka.1995@gmail.com>
2014-07-31 23:59:25 +0400

    Adding several menu items

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 __author__ = 'PrimankaDEN'
-
 from Tkinter import *
+from ttk import Combobox
+
 from game import *
 
 
@@ -35,7 +36,7 @@ file_menu = Menu(menu)
 menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New pattern")
 file_menu.add_command(label="Open pattern")
-file_menu.add_comand(label="Save pattern as")
+file_menu.add_command(label="Save pattern as")
 file_menu.add_separator()
 file_menu.add_command(label="Quit", command=root.destroy)
 
@@ -43,47 +44,43 @@ edit_menu = Menu(menu)
 menu.add_cascade(label="Edit", menu=edit_menu)
 edit_menu.add_command(label="Undo")
 edit_menu.add_separator()
+edit_menu.add_command(label="Add object")
+edit_menu.add_command(label="Delete object")
+edit_menu.add_separator()
 edit_menu.add_command(label="Cut")
 edit_menu.add_command(label="Copy")
 edit_menu.add_command(label="Paste", state=DISABLED)
+edit_menu.add_separator()
+edit_menu.add_command(label="Properties")
+
+run_menu = Menu(menu)
+menu.add_cascade(label="Run", menu=run_menu)
+run_menu.add_command(label="Run", command=GameController.start())
+run_menu.add_command(label="Pause/Resume", command=GameController.pauseOrResume())
+run_menu.add_command(label="Stop", command=GameController.stop())
 
 frame = Frame(root)
 frame.grid()
 
-#Start button
-startBtn = Button(frame, text="Resume/Pause")
-def resumeBtnClicked(event):
-    print "Start/Stop button"
-    if (GameController.isStarted()):
-        GameController.pause()
-    else:
-        GameController.resume()
-startBtn.bind("<Button-1>", resumeBtnClicked)
-startBtn.grid(row=1, column=0)
+#On/off show path
+isPathShownCbtn = Checkbutton(frame, text="Show path")
+isPathShownCbtn.grid(row=1, column=4)
 
-#Add object button
-addObjBtn= Button(frame,text="Start")
-def startBtnClicked(event):
-    print "addObjBtnClicked"
-    GameController.start()
-addObjBtn.bind("<Button-1>", startBtnClicked)
-addObjBtn.grid(row=0,column=0)
+#Set delta time
+deltaTimeLabel = Label(frame, text="Delta time")
+deltaTimeLabel.grid(row=2, column=0)
+deltaTimeScale = Scale(frame, orient=HORIZONTAL, length=200, from_=0, to_=100, tickinterval=20, resolution=1)
+deltaTimeScale.grid(row=2, column=1)
 
-#Add object button
-addObjBtn= Button(frame,text="Add object")
-def addObjBtnClicked(event):
-    print "addObjBtnClicked"
-addObjBtn.bind("<Button-1>", addObjBtnClicked)
-addObjBtn.grid(row=2,column=0)
+#Set delay
+delayLabel = Label(frame, text="Delay")
+delayLabel.grid(row=3, column=0)
+delayScale = Scale(frame, orient=HORIZONTAL, length=200, from_=0, to_=100, tickinterval=20, resolution=1)
+delayScale.grid(row=3, column=1)
 
-#
-
-lbl = Label(frame, text="Label")
-lbl.grid(row=0, column=1)
-
-txt = Text(frame, width=30, height=6)
-txt.grid(row=1, column=3)
-txt.insert(AtInsert(), "Text. " * 20)
+#List of objects
+combobox = Combobox(frame, values=["Sun", "Earth", "Moon"], height=3)
+combobox.grid(row=1, column = 5)
 
 cbtn = Checkbutton(frame, text="Checkbutton")
 cbtn.grid(row=2, column=3)
@@ -94,7 +91,7 @@ lst.insert('end', "Item 1")
 lst.insert('end', "Item 2")
 
 radioframe = Frame(frame, relief=RAISED, borderwidth=2)
-radioframe.grid(row=2, column=1, rowspan=3)
+radioframe.grid(row=2, column=2, rowspan=3)
 
 wave = StringVar()
 wave.set("1")
